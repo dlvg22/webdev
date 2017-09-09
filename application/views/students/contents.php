79981a31021f4f118ec4cb555682602069550db7
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2017-09-10 04:00:45 +0700

    courses

--- application/views/students/contents.php
+++ application/views/students/contents.php
@@ -59,7 +59,7 @@ foreach($student as $s)
 </div>
 <div class="row">
 	<div class="col-md-12 col-xs-12 text-center footer ">
-		Copy &copy 2017 . bootSTRAP
+		Copy &copy 2017 . bootstrap
 	</div>
 
 </div>
---
+++ application/views/students/course.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+		</div>
+		<div class="col-md-8">
+		<thead>COURSE</thead>
+		<?php
+		$course =array('BSIT','BSCS');
+		foreach($course as $s){
+				echo '<tbody>
+				<tr>
+				<td>'.$s.'<td>
+				</tr>
+				</tbody>';
+			
+			
+		} ?>
+		</div>
+	</div>
+</div>
