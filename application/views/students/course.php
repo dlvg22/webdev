bf528422b46fe55f1ce432ab7badb9df7b7128a4
haroldnicole <nclguiao@gmail.com>
2017-12-27 00:05:42 -0800

    Course view

--- application/views/students/course.php
+++ application/views/students/course.php
@@ -9,11 +9,11 @@
 		
 		<div class="btn-group">
 			<ul class="navbar">
+			<li>Student</li>
+			<li>Courses</li>
 			
-			<li><a class="btn btn-primary " href="students/course" role="button">STUDENT</a>
-			<li><a class="btn btn-primary " href="students/course" role="button">COURSE</a></li>
-			<li><a class="btn btn-primary " href="students/course" role="button">MENU</a></li>
-			
+			<a class="btn btn-primary" href="students/course" role="button">COURSE</a>
+			<li>MENU</li>
 			
 			</ul>
 			</div>
@@ -30,7 +30,7 @@
 	<th class="success">LAST NAME</th>
 	<th class="info">FIRST NAME</th>
 	<th >MIDDLE NAME</th>
-	<th>COURSE</th>
+	<th>ACTIONS</th>
 	<th>SEX</th>
 	<th>ACTIONS</th>
 	</thead>
